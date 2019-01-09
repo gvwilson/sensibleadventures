34927701284c5dead4d688b93a775a55475074cc
Greg Wilson <gvwilson@third-bit.com>
2019-01-09 08:18:19 -0500

    Cumulative probabilities

--- shapers/prob.py
+++ shapers/prob.py
@@ -21,3 +21,16 @@ for v in values:
     accum_p = 100 * accum[v] / base
     miss = base - total
     print(f'| {v : 6} | {ways[v] : 14} | {ways_p : 8.1f}% | {accum[v] : 13} | {accum_p : 15.1f}% |')
+
+print()
+
+bonuses = range(-7, 9)
+upper = 3 * 6 + 3
+print('| Bonus | Roll Needed | # Ways to Win |  % Win  |')
+print('| ----- | ----------- | ------------- | ------- |')
+for bonus in bonuses:
+    needed = 11 - bonus
+    get = accum[upper - needed]
+    win = 100 * get / base
+    print(f'| {bonus : >+5} | {needed : 11} | {get : 13} | {win : 6.1f}% |')
+

bcbbe5882a05746f5d7cb965e146889ef6559ecf
Greg Wilson <gvwilson@third-bit.com>
2020-02-10 08:53:12 -0500

    Tracking progress over time

---
+++ madica-roo/plot.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import plotly.express as px
+
+data = pd.read_csv('words.csv')
+data['Words'] = data['Words'].cumsum()
+fig = px.line(data, x='Date', y='Words').update_traces(mode='lines+markers')
+fig.show()

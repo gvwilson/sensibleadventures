89b674c1a4f0aee562b6a0df524996a887b866a6
Greg Wilson <gvwilson@third-bit.com>
2020-09-10 15:18:41 -0400

    Adding Maddy Roo

---
+++ words.sh
@@ -0,0 +1,8 @@
+wc -w \
+   beneath-coriandel/beneath-coriandel.md \
+   booksters-apprentice/booksters-apprentice.md \
+   cloudherd/cloudherd.md \
+   maddy-roo/maddy-roo.md \
+   madica/madica.md \
+   nardica/nardica.md \
+   wrestler/wrestler.md
